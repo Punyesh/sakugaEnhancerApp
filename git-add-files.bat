7b97c76de9aadee31ecb5b6f8217a3bbd169b2d9
Punyesh Kumar <punyesh.kumar@outlook.com>
2026-09-11 10:13:59 +0530

    grid

---
+++ git-add-files.bat
@@ -0,0 +1,15 @@
+@echo off
+git add .expo
+git add assets
+git add src
+git add .gitignore
+git add .easignore
+git add app.json
+git add App.tsx
+git add eas.json
+git add index.ts
+git add package.json
+git add package-lock.json
+git add privacy.html
+git add README.md
+git add tsconfig.json
